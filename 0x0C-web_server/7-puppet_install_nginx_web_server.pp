5978f77647194ac1cf43572501fb07e6cadbc899
lloyd kioko <ch.abene254@gmail.com>
2023-04-27 01:06:08 +0300

    commit 2

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,20 +1,24 @@
-# Puppet manifest to install nginx
-package { 'nginx':
-  ensure => installed,
-}
+#Install Nginx web server
 
-file_line { 'aaaaa':
-  ensure => 'present',
-  path   => '/etc/nginx/sites-available/default',
-  after  => 'listen 80 default_server;',
-  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+exec { 'Install Nginx':
+  command => 'sudo apt -y install nginx',
+  path    => ['/usr/bin/', '/bin'],
 }
 
 file { '/var/www/html/index.html':
-  content => 'Hello World',
+  content => 'Hello World!',
 }
 
-service { 'nginx':
-  ensure  => running,
-  require => Package['nginx'],
+file_line { 'redirect':
+  ensure   => present,
+  path     => '/etc/nginx/sites-available/default',
+  after    => 'listen 80 default_server;',
+  line     => 'rewrite /redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+  multiple => true
+}
+
+exec { 'Restart':
+  require => Exec['Install Nginx'],
+  command => 'sudo service nginx restart',
+  path    => ['/usr/bin', '/bin', '/usr/sbin'],
 }
